e82941536befc70bf10427cff7a546837e4d2daa
Den-85-ukr <madden85@ukr.net>
2021-04-18 19:20:32 +0300

    beginning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import { Component } from 'react';
+import ContactsList from './components/ContactsList';
+//import AddNewContactForm from './components/AddNewContactForm';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ contacts: [...this.state.contacts, this.state.name] });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <span>Name</span>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <ContactsList contacts={this.state.contacts} name={this.state.name}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
+
+const ContactsList = ({ contacts, name }) => {    
+    return (      
+      <div>
+        <h1>Contacts</h1>
+        <ul>{contacts.map(contact => (
+            <li key={uuidv4()}>{name}</li>
+        ))}
+        </ul>
+      </div>
+  );
+};
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ContactsList;
